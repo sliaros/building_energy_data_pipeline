a31209156e53f3da6552ea6020b19c1f5e3661e4
SteliosLiaros <sliaros@eren.com.gr>
2025-02-23 09:57:44 +0200

    refactored data_extractor.py

--- src/data_extraction/data_extractor.py
+++ src/data_extraction/data_extractor.py
@@ -2,36 +2,65 @@ import os
 import logging
 from src.utility.zenodo_client import ZenodoClient
 from src.utility.file_utils import FileUtils
+from typing import Optional
 
 class DataExtractor:
-    def __init__(self, config, logger=None):
+    def __init__(self, config: dict, logger: Optional[logging.Logger] = None) -> None:
+        """
+        Initializes the DataExtractor.
+
+        Args:
+            config (dict): Configuration dictionary for the extractor.
+            logger (Optional[logging.Logger]): Logger instance for logging. A new logger is created if not provided.
+        """
         self._config = config
         self._logger = logger or logging.getLogger(self.__class__.__name__)
         self._logger.info('Data Extractor initiated')
 
-    def download_data_from_zenodo(self):
+    def download_data_from_zenodo(self) -> Optional[str]:
         """
         Downloads a file from Zenodo based on the configuration.
+        The file is downloaded to the 'raw_data_file_path' location specified in the configuration.
+        The file is downloaded from the Zenodo record specified by the 'record_id' in the configuration.
+        The file is identified by the 'file_name' specified in the configuration.
 
-        Returns:
-            str: The path to the downloaded file, or None if the download failed.
+        The method returns the path to the downloaded file, or None if an error occurs.
         """
-        _record_id = self._config['project_data']['record_id']
-        _file_name = self._config['project_data']['file_name']
-        _folder_path = os.path.normpath(self._config['project_data']['raw_data_file_path'])
+        config: dict = self._config['project_data']
 
+        if not config:
+            raise ValueError("No configuration found for data extraction")
+
+        # Create a ZenodoClient to communicate with the Zenodo API
+        zenodo_client: ZenodoClient = ZenodoClient()
+
+        # Download the file from Zenodo
         try:
-            _zclient = ZenodoClient()
-            downloaded_file_path = _zclient.download_file_from_zenodo( _record_id, _file_name, _folder_path)
-            self._downloaded_file_path = downloaded_file_path
-        except Exception as e:
+            self._downloaded_file_path: str = zenodo_client.download_file_from_zenodo(
+                # The ID of the Zenodo record that contains the file
+                config['record_id'],
+                # The name of the file to download
+                config['file_name'],
+                # The path to the folder where the file will be saved
+                os.path.normpath(config['raw_data_file_path'])
+            )
+
+            # Return the path to the downloaded file
+            return self._downloaded_file_path
+        except (KeyError, TypeError) as e:
             self._logger.critical(f"Error downloading file from Zenodo: {e}")
             return None
 
     def unzip_downloaded_data(self):
+        # Retrieve the list of folder names to extract from the configuration
+        folder_names = self._config['project_data'].get('zip_file_folders_to_extract', [])
+
         try:
-            FileUtils().unzip_folders(self._downloaded_file_path, None,
-                self._config['project_data']['zip_file_folders_to_extract'])
+            # Attempt to unzip the downloaded file into the specified folders
+            # If folder_names is empty, all files will be extracted
+            FileUtils().unzip_folders(self._downloaded_file_path, None, folder_names)
         except Exception as e:
+            # Log any exceptions that occur during the unzipping process as critical errors
             self._logger.critical(f"Unhandled Exception: {e}")
+            # Return None to indicate that the operation was unsuccessful
             return None
